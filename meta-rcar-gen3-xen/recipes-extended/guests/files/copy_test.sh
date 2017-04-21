75ca5db3ae0b65e5f60a26755e6a5515a9658bd9
Andrii Anisov <andrii_anisov@epam.com>
2017-04-21 20:30:19 +0300

    Update copy test load script endless.
    In order to get possible long runs of the copy load test,
    make the script true endless.
    
    Signed-off-by: Andrii Anisov <andrii_anisov@epam.com>

--- meta-rcar-gen3-xen/recipes-extended/guests/files/copy_test.sh
+++ meta-rcar-gen3-xen/recipes-extended/guests/files/copy_test.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-dd if=/dev/xvda1 of=/home/root/test_1.img bs=512 count=10000000 &
-dd if=/dev/xvda2 of=/home/root/test_2.img bs=512 count=10000000 &
+
+function endless_copy {
+    while :
+    do
+        dd if=$1 of=$2 bs=512
+        rm $2
+    done
+}
+
+endless_copy /dev/xvda1 /home/root/test1.img &
+endless_copy /dev/xvda2 /home/root/test2.img &
